c69635dc51c5ec7c0be33b24e80f191acccd68c7
lwmfjc <lwmfjc@gmail.com>
2023-09-28 11:30:03 +0800

    删除注释

--- src/main/java/com/ly/leetcode/algorithm_and_interview_skills/Main.java
+++ src/main/java/com/ly/leetcode/algorithm_and_interview_skills/Main.java
@@ -1,7 +1,7 @@
 package com.ly.leetcode.algorithm_and_interview_skills;
-//这是一个中文输入法的测试
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
     }
-}//dskjfk是的开发健康this is a te st 测试卡就是看阿斯顿发
+}
